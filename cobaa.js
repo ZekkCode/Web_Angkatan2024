1eb7080f43e1d0b361cd1b52d79997ad4b1349b4
Zakaria MP <zakariamujur6@gmail.com>
2024-09-08 23:36:49 +0700

    update terbaru

--- cobaa.js
+++ cobaa.js
@@ -13,31 +13,48 @@ function getData() {
 
   async function friendListsElement() {
     const friendLists = document.getElementById("friend_list");
-    const data = await getData() 
-    const lists = `
-      ${data.map(i => `
-        <li>
-          <figure class="card">
-            <div class="card_header">
-              <div class="card_image">
-                <img src="${i.fotoselfie}" alt="${i.nama}" width="100%">
-              </div>
-              <div class="card_description">
-                <span>${i.nama}</span>
-                <span>${parseInt(i.nim)}</span>
-              </div>
-            </div>
-            <figcaption class="card_content">
-              <span>TTL : ${i.ttl}</span>
-              <span>Alamat : ${i.alamat}</span>
-              <span>No HP : ${i.no}</span>
-            </figcaption>
-          </figure>
-        </li>
-      `).join("")}
-    `;
-  
-    return friendLists.innerHTML = lists
+    const data = await getData();
+    
+    function renderList(items) {
+      if (items.length === 0) {
+        friendLists.innerHTML = '<li class="error-message">INGAT NAMANYA YANG BENER! KATANYA SE-ANGKATAN!!.</li>';
+      } else {
+        const lists = `
+          ${items.map(i => `
+            <li>
+              <figure class="card">
+                <div class="card_header">
+                  <div class="card_image">
+                    <img src="${i.fotoselfie}" alt="${i.nama}" width="100%">
+                  </div>
+                  <div class="card_description">
+                    <span>${i.nama}</span>
+                    <span>${parseInt(i.nim)}</span>
+                  </div>
+                </div>
+                <figcaption class="card_content">
+                  <span>TTL : ${i.ttl}</span>
+                  <span>Alamat : ${i.alamat}</span>
+                  <span>No HP : <a href="https://wa.me/62${i.no.replace(/^0|[^0-9]/g, '')}" target="_blank">${i.no}</a></span>
+                </figcaption>
+              </figure>
+            </li>
+          `).join("")}
+        `;
+        friendLists.innerHTML = lists;
+      }
+    }
+
+    renderList(data);
+
+    const searchInput = document.getElementById('searchInput');
+    searchInput.addEventListener('input', function() {
+      const searchTerm = this.value.toLowerCase();
+      const filteredData = data.filter(item => 
+        item.nama.toLowerCase().includes(searchTerm)
+      );
+      renderList(filteredData);
+    });
   }
-  
+
   friendListsElement()
